2d49c9655c8289f6b8389019383620c15c953f0b
z824583494-star <z824583494@gmail.com>
2026-07-18 00:18:37 +0800

    Update MainActivity.kt

--- app/src/main/java/com/videoflow/app/MainActivity.kt
+++ app/src/main/java/com/videoflow/app/MainActivity.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
